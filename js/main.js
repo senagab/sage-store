2a1519f16eb5b2322791c731d0248330f219e92b
senagab <senagab@gmail.com>
2023-10-05 15:05:52 -0400

    feat: sessão de contato

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.gallery img').hover(function() {
-        $(this).css('transform', 'scale(1.2)'); /* Adjust the scaling factor for desired zoom level */
-    }, function() {
-        $(this).css('transform', 'scale(1)');
-    });
-});
